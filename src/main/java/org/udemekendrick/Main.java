5d0d5563223bd6c7e02bb8f80f6982cc5d218556
UdemeKen <udemekendrick@gmail.com>
2024-05-28 17:22:33 +0100

    first commit

---
+++ src/main/java/org/udemekendrick/Main.java
@@ -0,0 +1,9 @@
+package org.udemekendrick;
+
+import org.udemekendrick.codewars.WhoLikesIt;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(WhoLikesIt.whoLikesIt());
+    }
+}
