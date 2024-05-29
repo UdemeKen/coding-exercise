ec9d383bbe35d17579231a8256eb28b4f0bdd8d6
UdemeKen <udemekendrick@gmail.com>
2024-05-29 09:29:17 +0100

    median sorted arrays

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,16 +1,14 @@
 package org.udemekendrick;
 
-import org.udemekendrick.codewars.WhoLikesIt;
 import org.udemekendrick.hackerrank.JavaStringIntroduction;
+import org.udemekendrick.leetcode.MedianTwoSortedArrays;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String A = scan.next();
-        String B = scan.next();
-
-        JavaStringIntroduction.stringManipulation(A, B);
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int[] nums2 = {6, 7, 8, 9, 10, 11};
+        System.out.println(MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2));
     }
 }
